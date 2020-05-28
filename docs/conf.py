4d5c1c4c6c66451be5ccf1b022b3503a9a03344a
Ali Youssef <aliali.mostafa99@gmail.com>
2020-05-28 13:49:22 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
-autodoc_mock_imports = ["Bio", "pandas"]
+autodoc_mock_imports = ["Bio", "pandas", "numpy", "gaft"]
 
 # -- Options for HTML output -------------------------------------------------
 
