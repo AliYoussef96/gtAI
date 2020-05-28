252bb333c61e759d959d330519c575fb90df6d81
Ali Youssef <aliali.mostafa99@gmail.com>
2020-05-28 13:37:48 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(r'../../gtAI'))
+sys.path.insert(0, os.path.abspath(r'../gtAI'))
 
 
 # -- Project information -----------------------------------------------------
