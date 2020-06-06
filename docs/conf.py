7a6d1a182557a223fdd7ab64d5ca69a17ffa6a8f
Ali Youssef <aliali.mostafa99@gmail.com>
2020-06-06 09:39:51 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ autodoc_mock_imports = ["Bio", "pandas", "numpy", "gaft"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
 
 
 # Sort members by type
